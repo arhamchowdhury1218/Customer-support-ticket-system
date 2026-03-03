f35992fbb3d01eeb588df8f2c29eebde0d73fac3
ärhamchowdhury1218 <arhamchowdhury1218@gmail.com>
2026-03-04 02:05:40 +0600

    completed navbar section including mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="max-w-full md:w-11/12 mx-auto">
+      <Navbar></Navbar>
       <h1 className="text-2xl">Vite + React</h1>
     </div>
   );
